41a98628f12f9f39a004b77e011bfef0f04f0ff8
Maksym <maksymgithub@gmail.com>
2016-07-08 02:21:05 -0700

    fixed typo

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,7 +1,7 @@
 import { CLIENT_ID, REDIRECT_URI } from '../constants/auth';
 
 export function auth() {
-  SC.initalize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
+  SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
 
   SC.connect().then((session) => {
     fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
