bf13e9efd465fe0bf57a62ba249e4e139b889910
Maksym <maksymgithub@gmail.com>
2016-07-09 18:34:44 -0700

    add thunk to middleware

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -8,7 +8,7 @@ import rootReducer from '../reducers/index';
 const logger = createLogger();
 const router = routerMiddleware(browserHistory);
 
-const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk, router, logger)(createStore);
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleware(rootReducer, initialState);
