6ad0a00b15e44aacb12ed4b783cb85d7430e4c30
Maksym <maksymgithub@gmail.com>
2016-07-09 18:47:23 -0700

    exchange login button once authenticated

--- src/components/Stream/index.js
+++ src/components/Stream/index.js
@@ -5,6 +5,7 @@ import * as actions from '../../actions';
 import Stream from './presenter';
 
 function mapStateToProps(state) {
+  const { user } = state.auth;
   const tracks = state.track;
   return {
     tracks
